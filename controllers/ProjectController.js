56574ae39c92985c878d20cdced8c05d2acdfca1
alamincoders <developeralamin03@gmail.com>
2022-09-23 22:17:36 +0600

    middleware and error handler and async handler setup

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -1,30 +1,35 @@
+const asyncHandler = require("express-async-handler");
 // @desc    Get projects
 // @route   GET /api/projects
 // @access  Private
-const getProjects = (req, res) => {
+const getProjects = asyncHandler(async (req, res) => {
   res.json({ message: "Loaded  projects" });
-};
+});
 
 // @desc    Create projects
 // @route   POST /api/projects
 // @access  Private
-const createdProjects = (req, res) => {
+const createdProjects = asyncHandler(async (req, res) => {
+  if (!req.body.name) {
+    res.status(400);
+    throw new Error("Please add a name field");
+  }
   res.json({ message: "Created  projects" });
-};
+});
 
 // @desc    Update project
 // @route   PUT /api/projects/:id
 // @access  Private
-const updatedProjects = (req, res) => {
+const updatedProjects = asyncHandler(async (req, res) => {
   res.json({ message: `Updated projects ${req.params.id}` });
-};
+});
 
 // @desc    Delete project
 // @route   DELETE /api/projects/:id
 // @access  Private
-const deletedProjects = (req, res) => {
+const deletedProjects = asyncHandler(async (req, res) => {
   res.json({ message: `deleted projects ${req.params.id}` });
-};
+});
 
 module.exports = {
   getProjects,
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/ErrorMiddleware");
 const cors = require("cors");
 
 const app = express();
@@ -8,7 +9,9 @@ const port = process.env.PORT || 8000;
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/projects", require("./routes/projectsRoutes"));
+app.use(errorHandler);
 
 // listen
 app.listen(port, () => {
