67a00f6d3fa7b01e232d6cb4c4eb4ab5de00704f
alamincoders <developeralamin03@gmail.com>
2022-09-25 21:44:48 +0600

    category field change

--- models/project.js
+++ models/project.js
@@ -14,10 +14,10 @@ const projectSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    category: {
+    category: [{
       type: String,
       required: true,
-    },
+    }],
     features: [
       {
         type: String,
